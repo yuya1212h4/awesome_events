705cfe77977e9c8a26425659173f2e832958ecac
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-08-25 20:18:45 +0900

    create start_time & end_time helpers
    日付の修正についてのヘルパーの作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def start_time(event)
+    event.start_time.strftime('%Y/%m/%d %H:%M')
+  end
+
+  def end_time(event)
+    event.end_time.strftime('%Y/%m/%d %H:%M')
+  end
 end
